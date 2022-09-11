65973f4d21e26281196591df0bc6a7dfc8f9750d
d.khristyukha <d.khristyukha@gmail.com>
2022-09-11 13:49:59 +0300

    Added pixel-generator module. Deleted default layout

---
+++ src/app/pixel-generator/pixel-generator.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PixelGeneratorRoutingModule } from "./pixel-generator-routing.module";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PixelGeneratorRoutingModule
+  ]
+})
+export class PixelGeneratorModule { }
